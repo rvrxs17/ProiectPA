71cee872f63eacfe0a7764667a8c3ecac5fb8eae
Andreiivascu290 <ivascuai2@yahoo.com>
2023-01-23 21:05:19 +0200

    slideShow

--- pagini/index.php
+++ pagini/index.php
@@ -11,7 +11,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
             <meta charset="utf-8">
             <link rel="stylesheet" href="index.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/> 
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="style.css">
         </head>
         <body>
             <nav>
@@ -24,16 +25,38 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
                 <li><a class="active" href="#">Acasa</a></li>
                 <li><a href = "http://localhost/ProiectPA/pagini/programari.php">Programari</a></li>
                 <li><a href="http://localhost/ProiectPA/pagini/desprenoi.php">Despre noi</a></li>
-                <li><a href="#">Servicii</a></li>
+                <li><a href="http://localhost/ProiectPA/pagini/servicii.php">Servicii</a></li>
                 <li><a href="http://localhost/ProiectPA/pagini/galerie.php">Galerie</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
             </nav>
-            <div class="content">
-                        <div>A.C.S. Arcasii Tarii Barsei
-                            <br> INDEXPHP
-                </div>
-            </div>
+            <br>
+            <div class="slideshow-container">
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="http://localhost/ProiectPA/pagini/photo/Archerygroup2.png" style="width:100%">
+            <div class="text">Teambuilding</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="http://localhost/ProiectPA/pagini/photo/echipament2.jpg" style="width:100%">
+            <div class="text">Echipament</div>
+        </div>
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="http://localhost/ProiectPA/pagini/photo/poza.jpg" style="width:100%">
+            <div class="text">Premiere Baile Tusnad 2022</div>
+        </div>
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+    <script src="script.js"></script>
         </body>
         </html>
 
